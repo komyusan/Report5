5fa507f372925d0395034001ca4abc60595c1d16
Yunosuke Komi <e235736@ie.u-ryukyu.ac.jp>
2023-12-04 18:28:14 +0900

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        try{
-            String str=null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str="壱百満";
+        int value;
+        value=Integer.parseInt(str);
     }
 
 }
